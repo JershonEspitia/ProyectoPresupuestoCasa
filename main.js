71a5a733f6481096518265ec8e584c862a8c9e68
JershonEspitia <jershonespitiarey@gmail.com>
2023-09-21 07:40:10 -0500

    feat: :sparkles: Codigo agregado
    Se termino la fiuncion buscar, se agrego el balance

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ addEventListener("DOMContentLoaded", async () => {
   let data = await (
     await fetch("https://6509d046f6553137159c1074.mockapi.io/Data")
   ).json();
+
+  let totalIngresos = 0;
+  let totalEgresos = 0;
+
   data.forEach((element) => {
     let tr = document.createElement("TR");
     tr.insertAdjacentHTML(
@@ -29,8 +33,31 @@ addEventListener("DOMContentLoaded", async () => {
       `
     );
     myTable.append(tr);
+
+    if (element.tipo === "Ingreso") {
+      totalIngresos = totalIngresos + element.valor;
+    } else {
+      totalEgresos = totalEgresos + element.valor;
+    }
+
   });
 
+  console.log(totalEgresos, totalIngresos);
+  // MOSTRAR INFORMACION DE BALANCE
+  const myTableBalance = document.querySelector("#table-balance");
+  let tr2 = document.createElement("TR");
+  tr2.insertAdjacentHTML(
+    "afterbegin",
+    ` 
+      <td>${totalIngresos}</td>
+      <td>${totalEgresos}</td>
+      <td>${totalIngresos - totalEgresos}</td>
+    `
+  );
+  myTableBalance.append(tr2);
+  
+
+  // ELIMINAR
   // Variable que almacena todos los botones que tengan la clase .btn-eliminar del documento
   const btn_delete = document.querySelectorAll(".btn-eliminar");
   // Ciclo que le asigna el evento de escucha a todos los botones de la constante btn_delete
@@ -44,6 +71,7 @@ addEventListener("DOMContentLoaded", async () => {
     });
   });
 
+  // EDITAR
   // Variable que almacena todos los botones que tengan la clase .btn-editar del documento
   const btn_edit = document.querySelectorAll(".btn-editar");
   // Ciclo que le asigna el evento de escucha a todos los botones de la constante btn_edit
@@ -71,54 +99,64 @@ addEventListener("DOMContentLoaded", async () => {
   document
     .querySelector('button[type="submit"]')
     .addEventListener("click", async () => {
-      const id = document
-        .querySelector('button[type="submit"]')
-        .getAttribute("data-edit-id");
 
       const nuevoValor = document.getElementById("input-valor").value;
       const nuevoTipo = document.querySelector(
         'input[name="tipo"]:checked'
       ).value;
-
-      const newData = {
-        tipo: nuevoTipo,
-        valor: nuevoValor,
-      };
-
-      let res = await fetch(`${API}/${id}`, {
-        method: "PUT",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify(newData),
-      });
     });
 
-  // document
-  //   .getElementById("searchButton")
-  //   .addEventListener("click", async () => {
-  //     const searchId = document.getElementById("searchInput").value.trim();
 
-  //     let dataFound = await (
-  //       await fetch("https://6509d046f6553137159c1074.mockapi.io/Data")
-  //     ).json();
-  //     console.log(dataFound);
-  //     myTable.innerHTML = "";
+  // BUSCAR
+  document
+  .getElementById("searchButton")
+  .addEventListener("click", async (e) => {
+    e.preventDefault();
 
-  //     // data.forEach((element) => {
-  //     //   let tr = document.createElement("TR");
-  //     //   tr.insertAdjacentHTML(
-  //     //     "afterbegin",
-  //     //     ` 
-  //     //       <td>${element.id}</td>
-  //     //       <td>${element.tipo}</td>
-  //     //       <td>${element.valor}</td>
-  //     //       <td><button edit_id="${element.id}" type="button" class="btn btn-primary btn-editar">Editar</button> <button id="btn_delete" delete_id="${element.id}" type="button" class="btn btn-danger btn-eliminar">Eliminar</button></td>
-  //     //     `
-  //     //   );
-  //     //   myTable.append(tr);
-  //     // });
-  //   });
+    const searchId = document.getElementById("searchInput").value.trim();
+
+    // Realizar la solicitud a la API con el valor de búsqueda
+    let dataFound = await (
+      await fetch(`${API}/${searchId}`)
+    ).json();
+
+    // Limpiar la tabla eliminando todo su contenido
+    myTable.innerHTML = "";
+
+    if (dataFound.id) {
+      // Si se encuentra un registro con el ID especificado, crear una fila en la tabla
+      let tr = document.createElement("TR");
+      tr.insertAdjacentHTML(
+        "afterbegin",
+        `
+          <td>${dataFound.id}</td>
+          <td>${dataFound.tipo}</td>
+          <td>${dataFound.valor}</td>
+          <td><button edit_id="${dataFound.id}" type="button" class="btn btn-primary btn-editar">Editar</button> <button id="btn_delete" delete_id="${dataFound.id}" type="button" class="btn btn-danger btn-eliminar">Eliminar</button></td>
+        `
+      );
+      myTable.append(tr);
+    } else {
+      // Si no se encuentra ningún registro, mostrar un mensaje de no encontrado
+      let tr = document.createElement("TR");
+      tr.insertAdjacentHTML(
+        "afterbegin",
+        `
+          <td colspan="4">No se encontraron resultados</td>
+        `
+      );
+      myTable.append(tr);
+    }
+  });
+
+  // LIMPIAR
+  document.getElementById("clearButton").addEventListener("click", async()=>{
+    window.location.reload();
+  });
+
 });
 
+// REGISTRAR
 // Evento que envia los datos ingresados a la API cuando se da en el boton
 myForm.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -128,12 +166,22 @@ myForm.addEventListener("submit", async (e) => {
 
   data.valor = typeof valor === "string" ? Number(valor) : null;
 
-  let res = await fetch(`${API}`, {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify(data),
-  });
+  const id = document
+        .querySelector('button[type="submit"]')
+        .getAttribute("data-edit-id");
 
-  console.log(res);
+  if (document.getElementById("btn-submit").textContent === "Registrar") {
+    let res = await fetch(`${API}`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    });
+  } else {
+    let res = await fetch(`${API}/${id}`, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    });
+  }
   window.location.reload();
 });
